3a762b04069e1cbd0d22943aea470a4135a3906e
Djordje Stevanovic <dsdjolence@gmail.com>
2018-10-16 13:19:56 +0200

    create react app

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Home extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className="application">
+        <h1>Hello Blockstack!</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './components/pages/Home';
+import * as serviceWorker from './serviceWorker';
+
+const element = document.getElementById('root');
+ReactDOM.render(<Home />, element);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
