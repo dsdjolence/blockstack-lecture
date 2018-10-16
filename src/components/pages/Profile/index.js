74799fe2bb4802da6e6e08efd197f978df57f097
Djordje Stevanovic <dsdjolence@gmail.com>
2018-10-16 14:36:52 +0200

    load user data

--- src/components/pages/Profile/index.js
+++ src/components/pages/Profile/index.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './styles.css';
-import {signUserOut} from 'blockstack';
+import {
+  loadUserData,
+  Person,
+  signUserOut
+} from 'blockstack'
+
+const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png'
 
 class Profile extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
-
+      person: {
+        name() {
+          return 'Anonymous'
+        },
+        avatarUrl() {
+          return avatarFallbackImage
+        },
+      },
     }
   }
 
@@ -16,11 +29,40 @@ class Profile extends Component {
     signUserOut(orign);
   }
 
+  componentWillMount() {
+    this.setState({
+      person: new Person(loadUserData().profile),
+    })
+  }
+
   render() {
+    const {person} = this.state
+
+    const name = () => {
+      if (!person.name()) {
+        return 'Nameless Person'
+      }
+
+      return person.name()
+    }
+
+    const avatar = () => {
+      if (!person.avatarUrl()) {
+        return avatarFallbackImage
+      }
+
+      return person.avatarUrl()
+    }
+
+    console.log(person);
     return (
         <div className="profile">
           <h1>Profile</h1>
           <button onClick={this.handleSignOut}>Sign out</button>
+          <div className="avatar-section">
+            <img alt={'Not found'} src={avatar()} className="avatar"/>
+          </div>
+          <h1>Hello, <span id="heading-name">{name()}</span>!</h1>
         </div>
     );
   }
